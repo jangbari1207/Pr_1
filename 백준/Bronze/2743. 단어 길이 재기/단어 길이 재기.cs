8f08857cdd3a1af4d403a270c0358739377f64d4
장호연 <ssjanghy@gmail.com>
2026-03-23 23:09:20 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 48 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s.Length);
+            
+        }
+    }
+}
