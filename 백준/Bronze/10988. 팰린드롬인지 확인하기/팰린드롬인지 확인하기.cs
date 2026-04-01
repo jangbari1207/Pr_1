31a3831b6454c951257aa25987830c52135c59a3
장호연 <ssjanghy@gmail.com>
2026-04-01 12:34:17 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 52 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cs
@@ -0,0 +1,20 @@
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        bool isPalindrome = true;
+
+        for (int i = 0; i < input.Length / 2; i++)
+        {
+            if (input[i] != input[input.Length - 1 - i])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(isPalindrome ? 1 : 0);
+    }
+}
